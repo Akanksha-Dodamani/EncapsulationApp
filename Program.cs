84d2d136415694e8edcc63371cae72eff1dff763
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-11 16:38:23 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductApp product1 = new ProductApp(1, "Mobile", 1000);
+            product1.DisplayProductDetails(product1);
+            ProductApp product2 = new ProductApp(2, "IPad", 500);
+            product2.DisplayProductDetails(product2);
+        }
+    }
+}
