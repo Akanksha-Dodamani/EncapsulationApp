9ca7c33956765ccd4834b90700b841f010d72522
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-17 10:40:13 +0530

    encapsulation of product app

--- ProductApp.cs
+++ ProductApp.cs
@@ -1,32 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EncapsulationApp
 {
     public class ProductApp
     {
-        public int id;
-        public string name;
-        public int price;
-        public readonly int DISCOUNT_PERCENTAGE = 20;
+        private int id;
+        private string name;
+        private double price;
+        private readonly int DISCOUNT_PERCENTAGE = 20;
 
+        public int GetId()
+        {
+            return id;
+        }
 
-        public ProductApp(int id, string name, int price)
+        public string GetName()
+        {
+            return name;
+        }
+
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public ProductApp(int id, string name, double price)
         {
             this.id = id;
             this.name = name;
             this.price = price;
         }
 
-        public int CalculateDiscountedPrice()
+        public double CalculateDiscountedPrice()
         {
             return price - (price * DISCOUNT_PERCENTAGE / 100);
         }
 
-        public  void DisplayProductDetails(ProductApp product)
+        public void DisplayProductDetails(ProductApp product)
         {
             Console.WriteLine($"Product ID: {product.id}");
             Console.WriteLine($"Product Name: {product.name}");
